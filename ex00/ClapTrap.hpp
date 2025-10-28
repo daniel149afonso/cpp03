e7dbf30acc392b41057e163037628756745fe3cb
daniel <daniel-congo2@hotmail.com>
2025-10-28 14:25:35 +0100

    created project

---
+++ ex00/ClapTrap.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daniel149afonso <daniel149afonso@studen    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/27 22:23:48 by daniel149af       #+#    #+#             */
+/*   Updated: 2025/10/27 22:25:19 by daniel149af      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAP_TRAP_H
+#define CLAP_TRAP_H
+
+#include <iostream>
+
+class ClapTrap
+{
+private:
+	/* data */
+public:
+	ClapTrap(/* args */);
+	~ClapTrap();
+};
+
+
