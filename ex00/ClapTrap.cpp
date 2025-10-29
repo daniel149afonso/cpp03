73b94108eee3c2c7ce2a18d7bed90c30ef405af5
daniel <daniel-congo2@hotmail.com>
2025-10-29 02:11:42 +0100

    orthodox canonical form implemented

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -6,16 +6,56 @@
 /*   By: daniel149afonso <daniel149afonso@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 22:25:22 by daniel149af       #+#    #+#             */
-/*   Updated: 2025/10/27 22:25:34 by daniel149af      ###   ########.fr       */
+/*   Updated: 2025/10/29 02:11:15 by daniel149af      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
-ClapTrap::ClapTrap(/* args */)
+// Constructors
+ClapTrap::ClapTrap(): _name("unknown"), _hit_pts(10), _energy_pts(10), _attack_dmg(0)
 {
+	std::cout << "Default Constructor called" << std::endl;
+}
+
+ClapTrap::ClapTrap(ClapTrap& n)
+{
+	_name= n._name;
+	_hit_pts = n._hit_pts;
+	_energy_pts = n._energy_pts;
+	_attack_dmg = n._attack_dmg;
+	std::cout << "Copy constructor called" << std::endl;
+}
+	
+	
+ClapTrap& ClapTrap::operator=(ClapTrap& n)
+{
+	if (this != &n)
+	{
+		_name= n._name;
+		_hit_pts = n._hit_pts;
+		_energy_pts = n._energy_pts;
+		_attack_dmg = n._attack_dmg;
+	}
+	std::cout << "Copy constructor called" << std::endl;
+	return (*this);
 }
 
 ClapTrap::~ClapTrap()
 {
+	std::cout << "Destructor called"<<std::endl;
+}
+	
+void attack(const std::string& target)
+{
+	"ClapTrap <name> attacks <target>, causing <damage> points of damage!\n";
+}
+void takeDamage(unsigned int amount)
+{
+	
+}
+
+void beRepaired(unsigned int amount)
+{
+	
 }
--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: daniel149afonso <daniel149afonso@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 22:23:48 by daniel149af       #+#    #+#             */
-/*   Updated: 2025/10/27 22:25:19 by daniel149af      ###   ########.fr       */
+/*   Updated: 2025/10/29 01:56:40 by daniel149af      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,20 @@
 class ClapTrap
 {
 private:
-	/* data */
+	std::string _name;
+	int _hit_pts = 10;
+	int _energy_pts = 10;
+	int _attack_dmg = 0;
 public:
-	ClapTrap(/* args */);
+	ClapTrap();
+	ClapTrap(int hit_pts, int energy_pts, int attack_dmg);
+	ClapTrap(ClapTrap& other);
+	ClapTrap& operator=(ClapTrap& other);
 	~ClapTrap();
+
+	void attack(const std::string& target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
 };
 
-
+#endif
